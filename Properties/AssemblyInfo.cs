5ab61b0e33d1db1234d3acd4d4a37bd7adafa7dd
JoshSp029 <99697200+JoshSp029@users.noreply.github.com>
2023-07-19 14:39:35 -0600

    Agregar archivos de proyecto.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChangeCalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChangeCalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5361d372-da00-4f9c-bd94-37803166442c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ChangeCalculatorTests
+{
+    [TestClass]
+    public class ChangeCalculatorTests
+    {
+        [TestMethod]
+        public void CalculateChange_Should_Return_Correct_Denominations()
+        {
+            // Arrange
+            ChangeCalculator changeCalculator = new ChangeCalculator("US");
+            double price = 10.50;
+            List<double> payment = new List<double> { 20.00, 5.00, 1.00 };
+
+            // Act
+            Dictionary<double, int> change = changeCalculator.CalculateChange(price, payment);
+
+            // Assert
+            Dictionary<double, int> expectedChange = new Dictionary<double, int>
+        {
+            { 5.00, 1 },
+            { 1.00, 2 },
+            { 0.25, 2 },
+            { 0.10, 2 },
+            { 0.05, 1 }
+        };
+
+            CollectionAssert.AreEqual(expectedChange, change);
+        }
+
+        [TestMethod]
+        public void CalculateChange_Should_Throw_Exception_On_Insufficient_Payment()
+        {
+            // Arrange
+            ChangeCalculator changeCalculator = new ChangeCalculator("Mexico");
+            double price = 100.00;
+            List<double> payment = new List<double> { 50.00 };
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => changeCalculator.CalculateChange(price, payment));
+        }
+
+        // Add more test methods as needed to cover various scenarios
+    }
+
+}
